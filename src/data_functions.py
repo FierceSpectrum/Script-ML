8be34f2d457f72491c6d8223e04474c33b319d7a
Benjamín Sandí <127576883+FierceSpectrum@users.noreply.github.com>
2024-12-06 09:34:05 -0600

    Update all

--- src/data_functions.py
+++ src/data_functions.py
@@ -1,10 +1,14 @@
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
 from logging_config import get_logger
 from dotenv import load_dotenv
 from data_connection import load_data
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
 
 logger = get_logger()
 
@@ -13,7 +17,6 @@ load_dotenv()
 
 
 def create_data_path(name_path):
-
     """
     Valida y crea la ruta hacia un archivo en la carpeta de "data".
 
@@ -44,6 +47,12 @@ def save_data(data, file_path, **kwargs):
     Guarda un DataFrame en un archivo CSV y confirma la acción.
     """
     try:
+        # Crear el directorio si no existe
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            logger.info(f"Directorio creado: {directory}")
+
         data.to_csv(file_path, index=False, **kwargs)
         logger.info(f"Archivo guardado: {file_path}")
         logger.info(f"Tamaño del conjunto: {data.shape}")
@@ -134,6 +143,214 @@ def split_data(data, train_size=0.65, val_size=0.30, test_size=0.05, random_stat
     return train_data, validation_data, test_data
 
 
+def create_image_path(name_path):
+    """
+    Crea la ruta completa donde se guardada las imagenes creadas mediante la libreria matplotlib.
+
+    Parámetros:
+        name_path (str): Nombre de la imagen a guardar.
+
+    Retorna:
+        Ruta hacia la carpeta "images" con el nombre de la imagen.
+    """
+
+    validate_env_variables('directory_image')
+    image_path = os.path.normpath(os.path.join(
+        os.getenv('directory_image'), name_path))
+
+    return image_path
+
+
+def plot_original_data(df, name_img="original_data_train.png"):
+    """
+    Grafico 3D original, el cual mantiene la estructura original de como se verian los datos.
+
+    Parámetros:
+        df: DataFrame de los datos agrupados por el modelo K-Means.
+        name_img: Nombre de la imagen que recibida el grafico al ser guardado.
+
+    Retorna:
+        Ejecuta una funcion ".show", donde muestra en una ventana un grafico 3D con los clusters agrupados.
+    """
+
+    graf = plt.figure(figsize=(10, 8))
+    ax = graf.add_subplot(111, projection='3d')
+    scatter = ax.scatter(df[:, 0], df[:, 1], df[:, 2],
+                         c="blue", s=50, label="Datos originales")
+
+    ax.set_title("Datos Originales")
+    ax.set_xlabel("total_day_minutes")
+    ax.set_ylabel("total_evening_minutes")
+    ax.set_zlabel("total_night_minutes")
+
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(create_image_path(name_img))
+    plt.show()
+
+
+def assign_clusters(df, labels):
+    """
+    Asigna las etiquetas de los cluster al conjunto de datos.
+
+    Parámetros:
+        df: DataFrame de los datos evaluados.
+        labels: Etiquetas de los datos evaluados.
+
+    Retorna:
+        Conjunto de datos con las etiquedas añadidas en la columna "Cluster".
+    """
+
+    df["Cluster"] = labels
+    return df
+
+
+def visualize_clusters_3d(data, labels, kmeans):
+
+    if data.shape[1] < 3:
+        raise ValueError(
+            "El conjunto de datos debe tener al menos 3 columnas para graficar en 3D.")
+
+    # Crear figura
+    fig = go.Figure()
+
+    # Obtener los centroides
+    centroids = kmeans.cluster_centers_
+
+    # Agregar puntos de cada cluster
+    unique_labels = np.unique(labels)
+    for cluster_id in unique_labels:
+        # Filtrar puntos del cluster actual
+        cluster_data = data[labels == cluster_id]
+        fig.add_trace(
+            go.Scatter3d(
+                x=cluster_data.iloc[:, 0],
+                y=cluster_data.iloc[:, 1],
+                z=cluster_data.iloc[:, 2],
+                mode='markers',
+                marker=dict(size=5, opacity=0.7),
+                name=f"Cluster {cluster_id}",
+                hoverinfo="x+y+z+text",
+            )
+        )
+
+    # Agregar centroides
+    fig.add_trace(
+        go.Scatter3d(
+            x=centroids[:, 0],
+            y=centroids[:, 1],
+            z=centroids[:, 2],
+            mode='markers+text',
+            marker=dict(size=10, color='red', symbol='cross'),
+            name="Centroides",
+            text=[f"Centroide {i}" for i in range(len(centroids))],
+            hoverinfo="text"
+        )
+    )
+
+    # Configurar el diseño
+    fig.update_layout(
+        title="Clusters visualizados en 3D (interactivo)",
+        scene=dict(
+            xaxis_title="total_day_minutes",
+            yaxis_title="total_evening_minutes",
+            zaxis_title="total_night_minutes"
+        ),
+        legend=dict(
+            x=0.1,
+            y=0.9,
+            title="Leyenda"
+        )
+    )
+
+    # Mostrar la figura
+    fig.show()
+
+
+def visualize_clusters_3d_matplotlib(data, labels, centroids, name_img="cluster_3d_train.png", title="Clusters visualizados en 3D"):
+    """
+    Visualiza clusters en 3D con matplotlib.
+
+    :param data: DataFrame o matriz con al menos 3 columnas (total_day_minutes, total_evening_minutes, total_night_minutes).
+    :param labels: Etiquetas de los clusters asignadas por K-Means.
+    :param kmeans: Modelo K-Means entrenado para acceder a los centroides.
+    """
+    if data.shape[1] < 3:
+        raise ValueError(
+            "El conjunto de datos debe tener al menos 3 columnas para graficar en 3D.")
+
+    # Configuración del gráfico
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Agregar puntos de cada cluster
+    unique_labels = np.unique(labels)
+    colors = plt.cm.viridis(np.linspace(0, 1, len(unique_labels)))
+
+    for cluster_id, color in zip(unique_labels, colors):
+        # Filtrar puntos del cluster actual
+        cluster_data = data[labels == cluster_id]
+        ax.scatter(
+            cluster_data.iloc[:, 0],
+            cluster_data.iloc[:, 1],
+            cluster_data.iloc[:, 2],
+            c=[color],
+            label=f"Cluster {cluster_id}",
+            s=50,
+            alpha=0.15
+        )
+
+    # Agregar centroides
+    ax.scatter(
+        centroids[:, 0],
+        centroids[:, 1],
+        centroids[:, 2],
+        c='red',
+        marker='X',
+        s=100,
+        label="Centroides"
+    )
+
+    # Configurar etiquetas y título
+    ax.set_title(title)
+    ax.set_xlabel("total_day_minutes")
+    ax.set_ylabel("total_evening_minutes")
+    ax.set_zlabel("total_night_minutes")
+
+    # Mostrar leyenda
+    ax.legend()
+    plt.grid(True)
+    plt.savefig(create_image_path(name_img))
+    plt.show()
+
+
+def plot_elbow_method(data, max_clusters=10, name_img="elbow_method.png"):
+    """
+    Grafica el método del codo para encontrar el número óptimo de clusters.
+    :param data: DataFrame con los datos normalizados.
+    :param max_clusters: Número máximo de clusters a probar.
+    """
+    validate_env_variables('random_state')
+    random_state = int(os.getenv('random_state'))
+
+    inertias = []
+
+    for k in range(1, max_clusters + 1):
+        kmeans = KMeans(n_clusters=k, random_state=random_state)
+        kmeans.fit(data)
+        inertias.append(kmeans.inertia_)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, max_clusters + 1), inertias, marker='o', linestyle='--')
+    plt.title("Método del Codo")
+    plt.xlabel("Número de Clusters")
+    plt.ylabel("Inercia")
+    plt.xticks(range(1, max_clusters + 1))
+    plt.grid(True)
+    plt.savefig(create_image_path(name_img))
+    plt.show()
+
+
 def main():
     """
     Función principal para cargar, procesar y dividir datos.
@@ -153,6 +370,7 @@ def main():
         validate_env_variables(
             'file_data',
             'file_data2',
+            'file_data3',
             'directory_data',
             'directory_project'
         )
@@ -195,13 +413,6 @@ def main():
         # Dividir datos
         train_data, validation_data, test_data = split_data(datos)
 
-        # Definir rutas de salida
-        output_files = {
-            'train': os.path.join(output_directory, 'train_data.csv'),
-            'validation': os.path.join(output_directory, 'validation_data.csv'),
-            'test': os.path.join(output_directory, 'test_data.csv')
-        }
-
         train_path = create_data_path('train_data.csv')
         val_path = create_data_path('validation_data.csv')
         test_path = create_data_path('test_data.csv')
@@ -210,6 +421,24 @@ def main():
         save_data(train_data, train_path)  # 65% de los datos
         save_data(validation_data, val_path)  # 30% de los datos
         save_data(test_data, test_path)  # 5% de los datos
+
+        # Cargar datos
+        datos = load_data(os.getenv('file_data3'))
+
+        # Filtrar columnas
+        datos = filter_columns(datos)
+
+        # Dividir datos
+        train_data2, validation_data2, test_data2 = split_data(datos)
+
+        train_path2 = create_data_path('train_data2.csv')
+        val_path2 = create_data_path('validation_data2.csv')
+        test_path2 = create_data_path('test_data2.csv')
+
+        # Guardar cada conjunto en archivos separados
+        save_data(train_data2, train_path2)  # 65% de los datos
+        save_data(validation_data2, val_path2)  # 30% de los datos
+        save_data(test_data2, test_path2)  # 5% de los datos
 
         # Confirmación final
         logger.info("Proceso de división de datos completado exitosamente")
--- src/gym.py
+++ src/gym.py
@@ -1,143 +1,212 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from dotenv import load_dotenv
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from data_connection import load_data
-import ml_functions as ml
 import seaborn as sns
+import data_functions as df
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import SpectralClustering, AgglomerativeClustering, MiniBatchKMeans, KMeans
+from sklearn.metrics import silhouette_score
+from dotenv import load_dotenv
+from sklearn.decomposition import PCA
+from data_connection import load_data
+from logging_config import get_logger
 
-
-
-from data_functions import validate_env_variables, save_data, create_data_path
-
+logger = get_logger()
 load_dotenv()
 
 
 def extract_data_path(name_file):
-    
     """
-    Extrae la ruta guardada en las variables de entorno y añade el nombre de un archivo a leer en la carpeta "data"
-
-    Parámetros:
-        name_file (str): Nombre del archivo a leer en la carpeta "data"
-
-    Retorna:
-        Ruta completa hacia un archivo
+    Extrae la ruta completa de un archivo de datos usando variables de entorno.
     """
-
-    validate_env_variables('directory_data')
-
+    df.validate_env_variables('directory_data')
     data_path = os.path.normpath(os.path.join(os.getenv(
         'directory_project'), os.path.join(os.getenv('directory_data'), name_file)))
     return data_path
 
 
-def model_K_Means():
-
-    # Valida la existencia de la variable de entorno antes de asignar su valor.
-    validate_env_variables('n_clusters')
-    n_clusters = int(os.getenv('n_clusters'))
-
-    # Ruta del archivo de datos
-    data_path = extract_data_path('train_data.csv')
-
-    # Cargar los datos
-    train_data = load_data(data_path)
-
-    # Entrenar modelo K-Means
-    kmeans_model, inertia, labels, silhouette, = ml.train_kmeans(
-        train_data, n_clusters=n_clusters)
-
-    explanation_percentage = ml.calculate_inertia_percentage(
-        train_data.values, inertia)
-
-    # Evaluar modelo
-    ml.evaluate_kmeans(inertia, silhouette)
-    ml.evaluate_kmeans(explanation_percentage, silhouette)
-
-    # Visualizar datos originales (normalizados)
-    # ml.plot_original_data(train_data.values, "kmeans_original_data_test.png")
-
-    # Visualizar clusters en 2D
-    # ml.visualize_clusters_3d(train_data, labels, kmeans_model)
-    # ml.visualize_clusters_3d_matplotlib(train_data, labels, kmeans_model, "kmeans_cluster_train.png")
-
-    # Asignar clusters a los datos originales
-    df_with_clusters = ml.assign_clusters(train_data.copy(), labels)
-    save_data(df_with_clusters, create_data_path('kmeans_train_data.csv'))
-
-    # # Visualizar el método del codo
-    # ml.plot_elbow_method(train_data.values, max_clusters=10, "kmeans_elbow_method.png")
-
-    # Validar el modelo con datos
-
-    # Ruta del archivo de datos
-    data_path = extract_data_path('validation_data.csv')
-
-    # Cargar los datos
-    validation_data = load_data(data_path)
-    validation_labels = ml.predict_kmeans(validation_data.values, kmeans_model)
-
-    # Asignar clusters a los datos originales
-    df_with_clusters = ml.assign_clusters(validation_data.copy(), validation_labels)
-    save_data(df_with_clusters, create_data_path('kmeans_validation_data.csv'))
-
-    # ml.plot_original_data(validation_data.values, "kmeans_original_data_validation.png")
-
-    # ml.visualize_clusters_3d(validation_data, validation_labels, kmeans_model)
-    # ml.visualize_clusters_3d_matplotlib(validation_data, validation_labels, kmeans_model,  "kmeans_cluster_validation.png")
-
-    silhouette = ml.calculate_silhouette_score(validation_data, validation_labels)
-    print("silhouette")
-    print(silhouette)
-
-    # Testear el modelo con datos
-
-    # Ruta del archivo de datos
-    data_path = extract_data_path('test_data.csv')
-
-    # Cargar los datos
-    test_data = load_data(data_path)
-    test_labels = ml.predict_kmeans(test_data.values, kmeans_model)
-
-    # Asignar clusters a los datos originales
-    df_with_clusters = ml.assign_clusters(test_data.copy(), test_labels)
-    save_data(df_with_clusters, create_data_path('kmeans_test_data.csv'))
-
-    # Se aplica modelo para medir la calidad de los cluster
-    silhouette = ml.calculate_silhouette_score(test_data, test_labels)
-    print("silhouette")
-    print(silhouette)
-
-    return
+def create_directory(rute_model):
+    """
+    Crea el directorio necesario para guardar los resultados del modelo.
+    """
+    directory_proyect = os.getcwd()
+    for dr in ['images', 'data']:
+        directory = os.path.normpath(os.path.join(
+            directory_proyect, f"{dr}/{rute_model}"))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            logger.info(f"Directorio creado: {directory}")
+    return directory
 
 
-def model_DBSCAN():
+def process_model(data, labels, model_name, rute_model, centroids, data_type):
+    """
+    Procesa un conjunto de datos, visualiza los resultados y guarda las métricas.
+    """
+    print(f"\nProcesando datos para {data_type}...\n")
 
-    # Valida la existencia de la variable de entorno antes de asignar su valor.
-    validate_env_variables('n_clusters')
-    n_clusters = int(os.getenv('n_clusters'))
+    # Calcular el coeficiente de silhouette
+    # labels deben ser las etiquetas, no el modelo
+    silhouette = silhouette_score(data, labels)
+    print(f"Silhouette score para {model_name}: {silhouette:.2f}")
 
-    # Ruta del archivo de datos
-    data_path = extract_data_path('train_data.csv')
-
-    # Cargar los datos
-    train_data = load_data(data_path)
-
-    # Entrenar modelo DBSCAN
-    dbscan = DBSCAN(eps=0.5, min_samples=5)
-    dbscan.fit(train_data)
-    train_labels = dbscan.labels_
-
+    # Asignar etiquetas y guardar resultados
+    df_with_clusters = df.assign_clusters(data.copy(), labels)
+    df.save_data(df_with_clusters, df.create_data_path(
+        f'{rute_model}/{data_type}_data.csv'))
     
-    return
+    df.plot_original_data(data, f"original_data_{data_type}.png")
+
+    # Visualizar los clusters en 3D
+    df.visualize_clusters_3d_matplotlib(data, labels, centroids, f"{
+                                         rute_model}/cluster_{data_type}.png", title=f"Distribución de clusters - {model_name}")
+
+    return silhouette
+
+
+def get_labels_and_centroids_fit(model_name, model, data, n_clusters):
+    """
+    Ajusta el modelo de clustering y obtiene las etiquetas y los centroides.
+    """
+    if model_name == "Gaussian Mixture Model":
+        labels = model.fit_predict(data)
+        centroids = model.means_
+    elif model_name == "K-Means Clustering":
+        model.fit(data)
+        labels = model.labels_
+        centroids = model.cluster_centers_
+    elif model_name == "MiniBatchKMeans":
+        labels = model.fit_predict(data)
+        centroids = model.cluster_centers_
+    else:
+        labels = model.fit_predict(data)
+        centroids = np.array([data[labels == i].mean(axis=0)
+                             for i in range(n_clusters)])
+
+    return labels, centroids
+
+
+def get_labels_and_centroids_predict(model_name, model, data, n_clusters):
+    """
+    Ajusta el modelo de clustering y obtiene las etiquetas y los centroides.
+    """
+    if model_name == "Gaussian Mixture Model":
+        labels = model.predict(data)
+        centroids = model.means_
+    elif model_name in ["K-Means Clustering", "MiniBatchKMeans"]:
+        labels = model.predict(data)
+        centroids = model.cluster_centers_
+    else:
+        labels = model.fit_predict(data)
+        centroids = np.array([data[labels == i].mean(axis=0)
+                             for i in range(n_clusters)])
+
+    return labels, centroids
+
+
+def process_clustering_model(data, model, model_name, rute_model, n_clusters=None):
+    """
+    Función para ajustar un modelo de clustering y calcular métricas, guardar resultados y visualizar.
+    """
+
+    # Ajustar el modelo
+    print(f"\nAjustando el modelo {model_name}...\n")
+
+    if not n_clusters:
+        df.validate_env_variables('n_clusters')
+        n_clusters = int(os.getenv('n_clusters'))
+
+    # Ajustar el modelo y obtener etiquetas y centroides
+    labels, centroids = get_labels_and_centroids_fit(
+        model_name, model, data, n_clusters)
+
+    # Procesar los datos de entrenamiento, validación y prueba
+    process_model(data, labels, model_name, rute_model,
+                  centroids, data_type="train")
+
+    # Procesar datos de validación
+    validation_data_path = extract_data_path('validation_data.csv')
+    validation_data = load_data(validation_data_path)
+    labels_v, centroids = get_labels_and_centroids_predict(
+        model_name, model, validation_data, n_clusters)
+    process_model(validation_data, labels_v, model_name,
+                  rute_model, centroids, data_type="validation")
+
+    # Procesar datos de prueba
+    test_data_path = extract_data_path('test_data.csv')
+    test_data = load_data(test_data_path)
+    labels_t, centroids = get_labels_and_centroids_predict(
+        model_name, model, test_data, n_clusters)
+    process_model(test_data, labels_t, model_name,
+                  rute_model, centroids, data_type="test")
+
+    return labels, labels_v, labels_t
+
+
+def model_clustering():
+    """
+    Entrena, evalúa y visualiza varios modelos de clustering (GMM, Spectral Clustering, Agglomerative y MiniBatchKMeans).
+    """
+    # Ruta del archivo de datos y carga
+    data_path = extract_data_path('train_data.csv')
+    train_data = load_data(data_path)
+
+    # Obtener parámetros de entorno
+    df.validate_env_variables('random_state', 'n_clusters')
+    random_state = int(os.getenv('random_state'))
+    n_clusters = int(os.getenv('n_clusters'))
+
+    # Modelos de clustering
+    models = {
+        "K-Means Clustering": KMeans(n_clusters=n_clusters, random_state=random_state),
+        "Gaussian Mixture Model": GaussianMixture(n_components=n_clusters, random_state=random_state),
+        "Spectral Clustering": SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors', random_state=random_state),
+        "Agglomerative Clustering": AgglomerativeClustering(n_clusters=n_clusters),
+        "MiniBatchKMeans": MiniBatchKMeans(n_clusters=n_clusters, random_state=random_state, batch_size=100)
+    }
+
+    model_rute = {
+        "K-Means Clustering": "kmeans",
+        "Gaussian Mixture Model": "gaussian",
+        "Spectral Clustering": "spectral",
+        "Agglomerative Clustering": "agglomerative",
+        "MiniBatchKMeans": "minikmeans"
+    }
+
+    # Procesar cada modelo
+    models_results = {}  # Initialize this variable to store results
+    for model_name, model in models.items():
+        labels, labels_t, labels_v = process_clustering_model(
+            train_data, model, model_name, model_rute[model_name])
+        models_results[model_name] = labels
+
+    # Visualizar el método del codo (solo para datos de entrenamiento)
+    df.plot_elbow_method(train_data.values, max_clusters=10,
+                          name_img="elbow_method.png")
+
+    print("\nModelado de Clustering completado.\n")
+
+    # Evaluación de métricas y comparación
+    metrics = []
+    for model_name, labels in models_results.items():
+        silhouette_avg = silhouette_score(train_data, labels)
+        cluster_counts = pd.Series(labels).value_counts().sort_index()
+
+        metrics.append({
+            "Model": model_name,
+            "Silhouette Score": silhouette_avg,
+            "Cluster Counts": cluster_counts.tolist()
+        })
+
+    # Mostrar métricas en tabla
+    metrics_df = pd.DataFrame(metrics)
+    print("\n### Comparación de resultados ###")
+    print(metrics_df)
 
 
 def main():
-    model_K_Means()
-    return
+    model_clustering()
 
 
 if __name__ == "__main__":
